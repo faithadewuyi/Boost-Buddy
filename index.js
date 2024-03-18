89063b35451cdd563925537b130c43a5caacfb18
faithadewuyi <iyanuleke09@gmail.com>
2024-03-18 13:08:45 +0100

    Updating data in real time

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref  } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push  } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js" //step 3 gotten standard import for real-time data
 
 const appSettings ={
   databaseURL: "https://endorse-b4353-default-rtdb.europe-west1.firebasedatabase.app/"
-}
+}//Step 1 URL copied from firebase
 
-const app =initializeApp(appSettings)
-const database = getDatabase(app)
+const app =initializeApp(appSettings)//Step 2
+const database = getDatabase(app)//step 2
 
 const endorsementListINDB = ref(database, "Endorsements")
 
 const messageEl = document.getElementById("message-el")
 const publishEl = document.getElementById("publish-el")
 const endorsementListEl =document.getElementById("endorsement-list")
+
+publishEl.addEventListener("click", function(){
+  let messageValue= messageEl.value
+
+  push(endorsementListINDB, messageValue)
+  endorsementListEl.innerHTML += `<p>${messageValue}</p>`
+
+})
